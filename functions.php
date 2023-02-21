148e01962bc017800d36a991588a994081a4722c
Gremza <info@gremza.com>
2023-02-21 15:02:07 +0100

    shtim i mega addon

--- functions.php
+++ functions.php
@@ -32,7 +32,19 @@ function lalutheme_register_required_plugins() {
 			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
 			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
 		),
+		array(
+			'name'               => 'Mega Addon for WPBakery', // The plugin name.
+			'slug'               => 'mega-addons', // The plugin slug (typically the folder name).
+			'source'             => get_template_directory() . '/plugins/mega.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '10.6.9', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
 		 
+	 
 	 
 		array(
 			'name'      => 'Classic Editor',
