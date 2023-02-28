20025af7c849e483d3de5c01b72cfbd3c80952a1
Gremza <info@gremza.com>
2023-02-28 11:29:31 +0100

    faqja no-titles eshte bere default

--- page-with_tites.php
+++ page-with_tites.php
@@ -1,12 +1,15 @@
-<?php get_header(); ?>  
+<?php
+/*
+Template Name: With header title
+*/
+get_header();  
+?>  
+ 
 
-<!-- header close -->
-
-<!-- content begin -->
-<div   class="container  page notit  ">
+	<div   class="container  page  ">
 	 
  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	  
+	  <div class="title"><h2><span><?php the_title();?></span></h2></div>
   
   <?php the_content();  ?> 
    
@@ -14,6 +17,5 @@
 	<p><?php _e('Sorry, this page does not exist.','lalutheme'); ?></p>
 	<?php endif; ?>
 	</div>
-	
 <?php get_footer();  ?>
 
